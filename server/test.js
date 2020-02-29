15f34b90ca2ce5e75d06d395dd5c001a638a7280
LeonGravel <leebroncc@gmail.com>
2020-02-29 16:02:29 +0800

    delete unused files

--- server/test.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-let app = http.createServer((req, res)=>{
-    res.end('hello king');
-})
-
-app.listen(3333);
--- src/pages/chat/wsconfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    host: 'ws://104.156.250.122',
-    port: 3000
-}
